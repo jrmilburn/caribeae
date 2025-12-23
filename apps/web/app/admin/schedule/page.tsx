2bc36b7edd852fae5ffc7d871872559adf75093d
jrmilburn <156278212+jrmilburn@users.noreply.github.com>
2025-12-24 08:13:06 +1000

    Add class instance fetching for schedule view

--- apps/web/app/admin/schedule/page.tsx
+++ apps/web/app/admin/schedule/page.tsx
@@ -1,4 +1,4 @@
-import { ScheduleView } from "@/packages/schedule"
+import { ScheduleView, createApiScheduleDataAdapter } from "@/packages/schedule"
 import { getLevels } from "@/server/level/getLevels"
 import { requireAdmin } from "@/lib/requireAdmin";
 
@@ -11,7 +11,7 @@ export default async function AdminSchedule() {
     return(
         <ScheduleView
             levels={levels}
-            dataAdapter={}
+            dataAdapter={createApiScheduleDataAdapter()}
         />
     )
 
