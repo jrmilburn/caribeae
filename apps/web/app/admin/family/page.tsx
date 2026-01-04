19c99c05fb503212b001b325a6c723ae7ba0202e
jrmilburn <156278212+jrmilburn@users.noreply.github.com>
2026-01-04 22:35:11 +1000

    Adjust family wizard actions

--- apps/web/app/admin/family/page.tsx
+++ apps/web/app/admin/family/page.tsx
@@ -1,19 +1,22 @@
 import { getFamilies } from "@/server/family/getFamilies"
 import { maybeRunInvoicingSweep } from "@/server/invoicing";
+import { getLevels } from "@/server/level/getLevels";
 
 import FamilyList from "./FamilyList";
 
 export default async function FamilyPage() {
 
     await maybeRunInvoicingSweep();
-    const [families] = await Promise.all([
+    const [families, levels] = await Promise.all([
       getFamilies(),
+      getLevels(),
     ]);
 
     return (
         <div className="">
         <FamilyList 
             families={families}
+            levels={levels}
         />
         </div>
     )
