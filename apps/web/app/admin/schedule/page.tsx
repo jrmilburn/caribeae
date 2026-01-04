d66ffef155ebc1bee46600b2ec97fdbea451d602
Joe Milburn <jrmilburn@outlook.com>
2026-01-04 15:51:22 +1000

    Implement get or create user before require admin on schedule page

--- apps/web/app/admin/schedule/page.tsx
+++ apps/web/app/admin/schedule/page.tsx
@@ -2,9 +2,11 @@ import { getLevels } from "@/server/level/getLevels"
 import { requireAdmin } from "@/lib/requireAdmin";
 import ScheduleWithTemplateModal from "./ScheduleWithTemplateModal";
 import { getTeachers } from "@/server/teacher/getTeachers";
+import { getOrCreateUser } from "@/lib/getOrCreateUser";
 
 export default async function AdminSchedule() {
 
+    await getOrCreateUser();
     await requireAdmin();
 
     const levels = await getLevels();
